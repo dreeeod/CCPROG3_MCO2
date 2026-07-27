c305a53663594854027c337ac50afdea4324d1f4
dreeeod <andeygamer@gmail.com>
2026-07-27 21:25:45 +0800

    Created new user-defined exceptions

---
+++ src/model/exceptions/LowRankException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class LowRankException extends Exception {
+    public LowRankException(String message) {
+        super(message);
+    }
+}
---
+++ src/model/exceptions/NegativeValueException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class NegativeValueException extends Exception {
+    public NegativeValueException(String message) {
+        super(message);
+    }
+}
